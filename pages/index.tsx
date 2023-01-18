696aa18c1b726f7c6baafe7947444771a397c6b6
Lucy Qiu <lucy1049684100@gmail.com>
2023-01-18 14:29:14 -0800

    favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home(): any {
 
               <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
                 <div className='flex'>
-                  <div className="sm:text-center lg:text-left">
+                  <div className="sm:text-center lg:text-left ml-6">
                     <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
                       <div className='flex'>
                         <span className="block xl:inline">Vote&nbsp; </span>
